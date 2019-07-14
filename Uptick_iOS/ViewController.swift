5e7218d88583789165c0c7a17225d4588e747119
Abdullah Al Mamun <amamun@u.rochester.edu>
2019-07-14 17:07:33 -0400

    Initial Commit

---
+++ Uptick_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uptick_iOS
+//
+//  Created by Abdullah Al Mamun on 7/14/19.
+//  Copyright © 2019 Abdullah Al Mamun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Uptick_iOSTests/Uptick_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Uptick_iOSTests.swift
+//  Uptick_iOSTests
+//
+//  Created by Abdullah Al Mamun on 7/14/19.
+//  Copyright © 2019 Abdullah Al Mamun. All rights reserved.
+//
+
+import XCTest
+@testable import Uptick_iOS
+
+class Uptick_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
